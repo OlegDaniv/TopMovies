1835eade49648e7632e0bdd481ef2d403217d360
AlexKr <duden89@gmail.com>
2022-09-09 12:31:25 +0200

    Test end line

--- app/src/main/java/com/example/topmovies/App.kt
+++ app/src/main/java/com/example/topmovies/App.kt
@@ -1,6 +1,7 @@
 package com.example.topmovies
 
 import android.app.Application
+import android.util.Log
 import com.example.topmovies.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -8,6 +9,7 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -15,5 +17,10 @@ class App : Application() {
             androidContext(this@App)
             modules(appModule)
         }
+        someMethod()
+    }
+
+    fun someMethod(){
+        Log.e("Logs", "App.someMethod() -> ${BuildConfig.VERSION_CODE}")
     }
 }
