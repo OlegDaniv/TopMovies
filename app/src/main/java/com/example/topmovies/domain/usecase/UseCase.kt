952397e6bae0234523f4a09c8e08c243f87f79f0
Oleg <oleg.daniv@skelia.com>
2023-01-06 13:03:07 +0200

    - renamed generics in UseCase.kt

--- app/src/main/java/com/example/topmovies/domain/usecase/UseCase.kt
+++ app/src/main/java/com/example/topmovies/domain/usecase/UseCase.kt
@@ -5,16 +5,16 @@ import com.example.topmovies.domain.utils.Failure
 import com.example.topmovies.domain.utils.ResultOf
 import java.util.concurrent.ExecutorService
 
-abstract class UseCase<Params, R> {
+abstract class UseCase<Params, Result> {
 
     abstract val executor: ExecutorService
     abstract val handler: Handler
 
-    abstract fun run(params: Params): ResultOf<Failure, R>
+    abstract fun run(params: Params): ResultOf<Failure, Result>
 
     operator fun invoke(
         params: Params,
-        onSuccess: (ResultOf<Failure, R>) -> Unit = {}
+        onSuccess: (ResultOf<Failure, Result>) -> Unit = {}
     ) {
         executor.execute {
             val result = run(params)
